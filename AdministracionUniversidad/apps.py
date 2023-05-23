ba5aea7cf5b512c0a7dbe54a1375f415fcba339d
TonyOG18A <j.olveda@uadec.edu.mx>
2023-05-23 12:13:02 -0600

    Initial commit

---
+++ AdministracionUniversidad/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AdministracionuniversidadConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'AdministracionUniversidad'
